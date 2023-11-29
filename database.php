14415ecfda859bf20776c5b8427a09e5ae81a2c5
Teddy Pascual <teddylayson2000@gmail.com>
2023-11-29 19:45:52 +0800

    added database and dynamic food menu

---
+++ database.php
@@ -0,0 +1,16 @@
+<?php
+
+$host = 'localhost'; // localhost or 127.0.0.1 pag ayaw gumana
+$user = 'root'; // usually root to lagi
+$password = ''; // database password, usually wala
+$database = 'posTry'; // database name
+$conn = '';
+
+try {
+    $conn = mysqli_connect($host, $user, $password, $database);
+} catch (mysqli_sql_exception) {
+    echo '<h1>Could Not Connect</h1>';
+}
+
+
+?>
--- pos.php
+++ pos.php
@@ -36,94 +36,37 @@
 
         </div>
 
-        <div class=" w-[100%] h-full grid grid-cols-4 grid-flow-row auto-rows-max gap-4 py-2 pl-2 overflow-y-scroll no-scrollbar ">
-       
-        <button class="rectangle relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900   rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
-          <span class="w-[100%] h-[100%] flex items-center justify-center text-xl relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 uppercase">
-          FRIED ITIK
-          <br>
-          (285)
-          </span>
-        </button>
-        <button class="rectangle relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900   rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
-          <span class="w-[100%] h-[100%] flex items-center justify-center text-xl relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 uppercase">
-          FRIED ITIK
-          <br>
-          (285)
-          </span>
-        </button>
-        <button class="rectangle relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900   rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
-          <span class="w-[100%] h-[100%] flex items-center justify-center text-xl relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 uppercase">
-          FRIED ITIK
-          <br>
-          (285)
-          </span>
-        </button>
-        <button class="rectangle relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900   rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
-          <span class="w-[100%] h-[100%] flex items-center justify-center text-xl relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 uppercase">
-          FRIED ITIK
-          <br>
-          (285)
-          </span>
-        </button>
-        <button class="rectangle relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900   rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
-          <span class="w-[100%] h-[100%] flex items-center justify-center text-xl relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 uppercase">
-          FRIED ITIK
-          <br>
-          (285)
-          </span>
-        </button>
-        <button class="rectangle relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900   rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
-          <span class="w-[100%] h-[100%] flex items-center justify-center text-xl relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 uppercase">
-          FRIED ITIK
-          <br>
-          (285)
-          </span>
-        </button>
-        <button class="rectangle relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900   rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
-          <span class="w-[100%] h-[100%] flex items-center justify-center text-xl relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 uppercase">
-          FRIED ITIK
-          <br>
-          (285)
-          </span>
-        </button>
-        <button class="rectangle relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900   rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
-          <span class="w-[100%] h-[100%] flex items-center justify-center text-xl relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 uppercase">
-          FRIED ITIK
-          <br>
-          (285)
-          </span>
-        </button>
-        <button class="rectangle relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900   rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
-          <span class="w-[100%] h-[100%] flex items-center justify-center text-xl relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 uppercase">
-          FRIED ITIK
-          <br>
-          (285)
-          </span>
-        </button>
-        <button class="rectangle relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900   rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
-          <span class="w-[100%] h-[100%] flex items-center justify-center text-xl relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 uppercase">
-          FRIED ITIK
-          <br>
-          (285)
-          </span>
-        </button>
-        <button class="rectangle relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900   rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
-          <span class="w-[100%] h-[100%] flex items-center justify-center text-xl relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 uppercase">
-          FRIED ITIK
-          <br>
-          (285)
-          </span>
-        </button>
-        <button class="rectangle relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900   rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
-          <span class="w-[100%] h-[100%] flex items-center justify-center text-xl relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 uppercase">
-          FRIED ITIK
-          <br>
-          (285)
-          </span>
-        </button>
+        <div id="food-div" class=" w-[100%] h-full grid grid-cols-4 grid-flow-row auto-rows-max gap-4 py-2 pl-2 overflow-y-scroll no-scrollbar ">
+            <?php
+                include("database.php");
+                $sql = "SELECT * FROM food";
+                $result = mysqli_query($conn, $sql);
+
+                if(mysqli_num_rows($result) > 0) { // if may result
+                    while ($row = mysqli_fetch_assoc($result)) {
+                        echo '<button class="rectangle relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900   rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
+                        <span class="w-[100%] h-[100%] flex items-center justify-center text-xl relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 uppercase">
+                        '.$row['name'].'
+                        <br>
+                        ('.$row['price'].')
+                        </span>
+                    </button>';
+                    }
+                } else { // kapag walang result
+                    echo '<h1>THERE ARE CURRENTLY NO STUDENTS </h1>';
+                }
+             ?>
+            <!-- <button class="rectangle relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900   rounded-lg group bg-gradient-to-br from-purple-600 to-blue-500 group-hover:from-purple-600 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800">
+                <span class="w-[100%] h-[100%] flex items-center justify-center text-xl relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0 uppercase">
+                FRIED ITIK
+                <br>
+                (285)
+                </span>
+            </button> -->
+            
         
         </div>
+
       </div>
     </div>
     <div class="content-height w-[40%] p-4">
