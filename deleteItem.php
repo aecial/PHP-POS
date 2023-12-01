d46edc1e4290472e3137ed1fbae9815267aa6f43
Teddy Pascual <teddylayson2000@gmail.com>
2023-12-01 17:35:46 +0800

    removed comments

--- deleteItem.php
+++ deleteItem.php
@@ -3,9 +3,5 @@
     $name = $_POST['name'];
     $sql = "DELETE FROM orders WHERE name='$name'";
     $result = mysqli_query($conn, $sql);
-    // if($result) {
-    //   echo "OK";
-    // } else {
-    //   echo "NOMMM";
-    // }
+
 ?>
