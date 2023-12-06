c79a2176d3b18dba7bd94047425c8d0bdf0070b5
Teddy Pascual <teddylayson2000@gmail.com>
2023-12-07 07:30:17 +0800

    Chartjs with PHP and Mysql

--- dashboard.php
+++ dashboard.php
@@ -9,23 +9,43 @@
 </head>
 <body>
     <?php include("./components/adminNav.php"); ?>
-    <main class="bg-slate-600 content-height justify-center items-center flex">
-    <div>
+    <main class="bg-slate-600 content-height justify-center items-center flex flex-col">
+    <h1 class="text-4xl text-white mb-4">Monthly Gross Sales</h1>
+    <div class="bg-white w-[60%] h-[70%]">
+        
         <canvas id="myChart"></canvas>
     </div>
 
     </main>
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <?php
+     include("database.php");
+     $prices = array();
+     $sql = "SELECT price FROM orders";
+     $result = mysqli_query($conn, $sql);
+     if(mysqli_num_rows($result) > 0) {
+       while ($row = mysqli_fetch_assoc($result)) {
+         $price = $row['price'];
+         array_push($prices, $price);
+       }
+       print_r($prices);
+       echo count($prices);
+     }
+     
+    ?>
     <script>
         const ctx = document.getElementById('myChart');
-
         new Chart(ctx, {
             type: 'bar',
             data: {
-            labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+            labels: [<?php for($i = 1; $i <= 31; $i++) {
+                echo "'".$i."',";
+            } ?>],
             datasets: [{
-                label: 'GROSS SALES',
-                data: [2000, 3000, 11000, 50000, 2000, 3000],
+                label: 'DECEMBER GROSS SALES',
+                data: [<?php for($i = 0; $i <= count($prices)-1; $i++) {
+                echo "'".$prices[$i]."',";
+            } ?>],
                 borderWidth: 1
             }]
             },
